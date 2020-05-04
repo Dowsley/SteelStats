e4f4c9fa1a96a29dbf13672dd371125234fe6f10
Dowsley <joao.dowsley850@gmail.com>
2020-05-04 15:29:16 -0300

    NOVO: Esqueleto Backend

---
+++ backend/src/test/java/g5/dashboard/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package g5.dashboard.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
