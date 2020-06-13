a9d3c5ff57e75b3540cbaade4835ddf4427b8241
viniciusrgf <61975112+viniciusrgf@users.noreply.github.com>
2020-06-12 21:01:06 -0300

    Add files via upload

---
+++ frontend/dashboard/src/components/views/spacing.vue
@@ -0,0 +1,25 @@
+  <template>
+  <div>
+    
+    <v-app id="inspire">
+    <v-card
+      class="ma-auto"
+      color="#fbfbfb"
+
+       border-radius:20px
+    >
+      <v-card-text>
+        //
+        //
+        //
+        Colocar o codigo do grafico aqui(na verdade não aqui mas
+        fora desse "v-card-text")
+        //
+        //
+        //
+      </v-card-text>
+    </v-card>
+  </v-app>
+
+  </div>
+  </template>
